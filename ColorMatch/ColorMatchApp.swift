be12d9628efe3ca9a6735973b2c35784e1e8868f
Mladen Raykov <raykovmladen@gmail.com>
2025-12-28 21:48:59 +0200

    Initial Commit

---
+++ ColorMatch/ColorMatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorMatchApp.swift
+//  ColorMatch
+//
+//  Created by Madji on 28.12.25.
+//
+
+import SwiftUI
+
+@main
+struct ColorMatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColorMatchTests/ColorMatchTests.swift
@@ -0,0 +1,17 @@
+//
+//  ColorMatchTests.swift
+//  ColorMatchTests
+//
+//  Created by Madji on 28.12.25.
+//
+
+import Testing
+@testable import ColorMatch
+
+struct ColorMatchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
